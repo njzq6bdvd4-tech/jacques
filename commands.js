e48a3f011d5c7af33a548d490ea2a3830555e727
njzq6bdvd4-tech <njzq6bdvd4@privaterelay.appleid.com>
2026-04-08 16:56:02 +0100

    Add files via upload
    upload most files

---
+++ commands.js
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+import { getRPSChoices } from './game.js';
+import { capitalize, InstallGlobalCommands } from './utils.js';
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Commande simple',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+// Command containing options
+const CHALLENGE_COMMAND = {
+  name: 'challenge',
+  description: 'Faire un défi pour jouer pierre papier ciseux avec qqun',
+  options: [
+    {
+      type: 3,
+      name: 'objet',
+      description: 'choisi un objet',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 2],
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
